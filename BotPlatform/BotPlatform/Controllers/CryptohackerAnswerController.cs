f3332bb522e9a2a3a0cda64f8ad31a63ff310e9f
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-02-10 19:53:39 +0200

    Ceate CryptohackerController and work with gender request

---
+++ BotPlatform/BotPlatform/Controllers/CryptohackerAnswerController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BotPlatform.Cryptohacker;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BotPlatform.Controllers
+{
+    [Route("api/[controller]")]
+    public class CryptohackerAnswerController : Controller
+    {
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(string id)
+        {
+            ChatAnswer answer = new ChatAnswer();
+            return answer.GetGenderAnswer(id);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+    }
+}
---
+++ BotPlatform/BotPlatform/Cryptohacker/ChatAnswer.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BotPlatform.Cryptohacker
+{
+    public class ChatAnswer
+    {
+        private List<string> mailAnswers;
+        private List<string> femailAnswers;
+
+        public ChatAnswer()
+        {
+            mailAnswers = new List<string>();
+            femailAnswers = new List<string>();
+            FillAnswers();
+        }
+
+        public string GetGenderAnswer(string gender)
+        {
+            if (gender == "mail")
+                return mailAnswers.ElementAt(Rnd(mailAnswers));
+            else return femailAnswers.ElementAt(Rnd(femailAnswers));
+        }
+
+        private void FillAnswers()
+        {
+            FillMailAnswers();
+            FillFemailAnswers();
+        }
+
+        private void FillMailAnswers()
+        {
+            mailAnswers.Add("Гей, ковбоє, полегше...");
+            mailAnswers.Add("Воув, хлопче, не так швидко...");
+            mailAnswers.Add("І що це ти написав?");
+        }
+
+        private void FillFemailAnswers()
+        {
+            femailAnswers.Add("Гей, кралечко, полегше...");
+            femailAnswers.Add("Воув, дівонька, не так швидко...");
+            femailAnswers.Add("І що це ти написала?");
+        }
+
+        private int Rnd(List<string> answList)
+        {
+            Random rnd = new Random();
+
+            return rnd.Next(answList.Count);
+        }
+    }
+}
