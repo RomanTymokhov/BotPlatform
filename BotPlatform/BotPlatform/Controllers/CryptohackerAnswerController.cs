2894893303efffbe4ec00dc7305df48b255af295
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-02-13 06:33:35 +0200

    Create true Get method

--- BotPlatform/BotPlatform/Controllers/CryptohackerAnswerController.cs
+++ BotPlatform/BotPlatform/Controllers/CryptohackerAnswerController.cs
@@ -11,38 +11,22 @@ namespace BotPlatform.Controllers
     [Route("api/[controller]")]
     public class CryptohackerAnswerController : Controller
     {
-        // GET api/values
         [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
-
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/values
-        [HttpGet]
-        public string Post([FromBody]Message msg)
+        public string Get()
         {
             ChatAnswer answer = new ChatAnswer();
-            return answer.GetGenderAnswer(msg.message);
+
+            string gender = Request.Query.FirstOrDefault(p => p.Key == "gender").Value;
+
+            return answer.GetGenderAnswer(gender);
         }
 
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
+        //// POST api/values
+        //[HttpGet]
+        //public string Post([FromBody]Message msg)
+        //{
+        //    //ChatAnswer answer = new ChatAnswer();
+        //    //return answer.GetGenderAnswer(msg.message);
+        //}
     }
 }
