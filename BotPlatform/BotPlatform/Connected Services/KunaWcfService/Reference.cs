bf43f725cbf46250383e3ef365a977eb3071def7
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-02-27 19:10:23 +0200

    add wcf service Kuna

---
+++ BotPlatform/BotPlatform/Connected Services/KunaWcfService/Reference.cs
@@ -0,0 +1,127 @@
+﻿//------------------------------------------------------------------------------
+// <автоматически создаваемое>
+//     Этот код создан программой.
+//     //
+//     Изменения в этом файле могут привести к неправильной работе и будут потеряны в случае
+//     повторного создания кода.
+// </автоматически создаваемое>
+//------------------------------------------------------------------------------
+
+namespace KunaWcfService
+{
+    
+    
+    [System.CodeDom.Compiler.GeneratedCodeAttribute("dotnet-svcutil", "1.0.0.0")]
+    [System.ServiceModel.ServiceContractAttribute(ConfigurationName="KunaWcfService.IService1")]
+    public interface IService1
+    {
+        
+        [System.ServiceModel.OperationContractAttribute(Action="http://tempuri.org/IService1/GetTimestamp", ReplyAction="http://tempuri.org/IService1/GetTimestampResponse")]
+        System.Threading.Tasks.Task<string> GetTimestampAsync();
+    }
+    
+    [System.CodeDom.Compiler.GeneratedCodeAttribute("dotnet-svcutil", "1.0.0.0")]
+    public interface IService1Channel : KunaWcfService.IService1, System.ServiceModel.IClientChannel
+    {
+    }
+    
+    [System.Diagnostics.DebuggerStepThroughAttribute()]
+    [System.CodeDom.Compiler.GeneratedCodeAttribute("dotnet-svcutil", "1.0.0.0")]
+    public partial class Service1Client : System.ServiceModel.ClientBase<KunaWcfService.IService1>, KunaWcfService.IService1
+    {
+        
+    /// <summary>
+    /// Реализуйте этот разделяемый метод для настройки конечной точки службы.
+    /// </summary>
+    /// <param name="serviceEndpoint">Настраиваемая конечная точка</param>
+    /// <param name="clientCredentials">Учетные данные клиента.</param>
+    static partial void ConfigureEndpoint(System.ServiceModel.Description.ServiceEndpoint serviceEndpoint, System.ServiceModel.Description.ClientCredentials clientCredentials);
+        
+        public Service1Client() : 
+                base(Service1Client.GetDefaultBinding(), Service1Client.GetDefaultEndpointAddress())
+        {
+            this.Endpoint.Name = EndpointConfiguration.BasicHttpBinding_IService1.ToString();
+            ConfigureEndpoint(this.Endpoint, this.ClientCredentials);
+        }
+        
+        public Service1Client(EndpointConfiguration endpointConfiguration) : 
+                base(Service1Client.GetBindingForEndpoint(endpointConfiguration), Service1Client.GetEndpointAddress(endpointConfiguration))
+        {
+            this.Endpoint.Name = endpointConfiguration.ToString();
+            ConfigureEndpoint(this.Endpoint, this.ClientCredentials);
+        }
+        
+        public Service1Client(EndpointConfiguration endpointConfiguration, string remoteAddress) : 
+                base(Service1Client.GetBindingForEndpoint(endpointConfiguration), new System.ServiceModel.EndpointAddress(remoteAddress))
+        {
+            this.Endpoint.Name = endpointConfiguration.ToString();
+            ConfigureEndpoint(this.Endpoint, this.ClientCredentials);
+        }
+        
+        public Service1Client(EndpointConfiguration endpointConfiguration, System.ServiceModel.EndpointAddress remoteAddress) : 
+                base(Service1Client.GetBindingForEndpoint(endpointConfiguration), remoteAddress)
+        {
+            this.Endpoint.Name = endpointConfiguration.ToString();
+            ConfigureEndpoint(this.Endpoint, this.ClientCredentials);
+        }
+        
+        public Service1Client(System.ServiceModel.Channels.Binding binding, System.ServiceModel.EndpointAddress remoteAddress) : 
+                base(binding, remoteAddress)
+        {
+        }
+        
+        public System.Threading.Tasks.Task<string> GetTimestampAsync()
+        {
+            return base.Channel.GetTimestampAsync();
+        }
+        
+        public virtual System.Threading.Tasks.Task OpenAsync()
+        {
+            return System.Threading.Tasks.Task.Factory.FromAsync(((System.ServiceModel.ICommunicationObject)(this)).BeginOpen(null, null), new System.Action<System.IAsyncResult>(((System.ServiceModel.ICommunicationObject)(this)).EndOpen));
+        }
+        
+        public virtual System.Threading.Tasks.Task CloseAsync()
+        {
+            return System.Threading.Tasks.Task.Factory.FromAsync(((System.ServiceModel.ICommunicationObject)(this)).BeginClose(null, null), new System.Action<System.IAsyncResult>(((System.ServiceModel.ICommunicationObject)(this)).EndClose));
+        }
+        
+        private static System.ServiceModel.Channels.Binding GetBindingForEndpoint(EndpointConfiguration endpointConfiguration)
+        {
+            if ((endpointConfiguration == EndpointConfiguration.BasicHttpBinding_IService1))
+            {
+                System.ServiceModel.BasicHttpBinding result = new System.ServiceModel.BasicHttpBinding();
+                result.MaxBufferSize = int.MaxValue;
+                result.ReaderQuotas = System.Xml.XmlDictionaryReaderQuotas.Max;
+                result.MaxReceivedMessageSize = int.MaxValue;
+                result.AllowCookies = true;
+                return result;
+            }
+            throw new System.InvalidOperationException(string.Format("Не удалось найти конечную точку с именем \"{0}\".", endpointConfiguration));
+        }
+        
+        private static System.ServiceModel.EndpointAddress GetEndpointAddress(EndpointConfiguration endpointConfiguration)
+        {
+            if ((endpointConfiguration == EndpointConfiguration.BasicHttpBinding_IService1))
+            {
+                return new System.ServiceModel.EndpointAddress("http://kunawcfservice/Service1.svc");
+            }
+            throw new System.InvalidOperationException(string.Format("Не удалось найти конечную точку с именем \"{0}\".", endpointConfiguration));
+        }
+        
+        private static System.ServiceModel.Channels.Binding GetDefaultBinding()
+        {
+            return Service1Client.GetBindingForEndpoint(EndpointConfiguration.BasicHttpBinding_IService1);
+        }
+        
+        private static System.ServiceModel.EndpointAddress GetDefaultEndpointAddress()
+        {
+            return Service1Client.GetEndpointAddress(EndpointConfiguration.BasicHttpBinding_IService1);
+        }
+        
+        public enum EndpointConfiguration
+        {
+            
+            BasicHttpBinding_IService1,
+        }
+    }
+}
