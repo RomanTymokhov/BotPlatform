211962dffcaa077b24baba0865373ec1cca06218
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-02-28 04:58:04 +0200

    остался не решенный вопрос с клиентом сервиса, почему то конструктор ребует параметры.

---
+++ BotPlatform/BotPlatform/Connected Services/KunaService/Reference.cs
@@ -0,0 +1,126 @@
+﻿//------------------------------------------------------------------------------
+// <автоматически создаваемое>
+//     Этот код создан программой.
+//     //
+//     Изменения в этом файле могут привести к неправильной работе и будут потеряны в случае
+//     повторного создания кода.
+// </автоматически создаваемое>
+//------------------------------------------------------------------------------
+
+namespace KunaService
+{
+    
+    
+    [System.CodeDom.Compiler.GeneratedCodeAttribute("dotnet-svcutil", "1.0.0.0")]
+    [System.ServiceModel.ServiceContractAttribute(ConfigurationName="KunaService.IKunaServise")]
+    public interface IKunaServise
+    {
+        
+        [System.ServiceModel.OperationContractAttribute(Action="http://tempuri.org/IKunaServise/GetTimestamp", ReplyAction="http://tempuri.org/IKunaServise/GetTimestampResponse")]
+        System.Threading.Tasks.Task<string> GetTimestampAsync();
+    }
+    
+    [System.CodeDom.Compiler.GeneratedCodeAttribute("dotnet-svcutil", "1.0.0.0")]
+    public interface IKunaServiseChannel : KunaService.IKunaServise, System.ServiceModel.IClientChannel
+    {
+    }
+    
+    [System.Diagnostics.DebuggerStepThroughAttribute()]
+    [System.CodeDom.Compiler.GeneratedCodeAttribute("dotnet-svcutil", "1.0.0.0")]
+    public partial class KunaServiseClient : System.ServiceModel.ClientBase<KunaService.IKunaServise>, KunaService.IKunaServise
+    {
+        
+    /// <summary>
+    /// Реализуйте этот разделяемый метод для настройки конечной точки службы.
+    /// </summary>
+    /// <param name="serviceEndpoint">Настраиваемая конечная точка</param>
+    /// <param name="clientCredentials">Учетные данные клиента.</param>
+    static partial void ConfigureEndpoint(System.ServiceModel.Description.ServiceEndpoint serviceEndpoint, System.ServiceModel.Description.ClientCredentials clientCredentials);
+        
+        public KunaServiseClient(EndpointConfiguration endpointConfiguration) : 
+                base(KunaServiseClient.GetBindingForEndpoint(endpointConfiguration), KunaServiseClient.GetEndpointAddress(endpointConfiguration))
+        {
+            this.Endpoint.Name = endpointConfiguration.ToString();
+            ConfigureEndpoint(this.Endpoint, this.ClientCredentials);
+        }
+        
+        public KunaServiseClient(EndpointConfiguration endpointConfiguration, string remoteAddress) : 
+                base(KunaServiseClient.GetBindingForEndpoint(endpointConfiguration), new System.ServiceModel.EndpointAddress(remoteAddress))
+        {
+            this.Endpoint.Name = endpointConfiguration.ToString();
+            ConfigureEndpoint(this.Endpoint, this.ClientCredentials);
+        }
+        
+        public KunaServiseClient(EndpointConfiguration endpointConfiguration, System.ServiceModel.EndpointAddress remoteAddress) : 
+                base(KunaServiseClient.GetBindingForEndpoint(endpointConfiguration), remoteAddress)
+        {
+            this.Endpoint.Name = endpointConfiguration.ToString();
+            ConfigureEndpoint(this.Endpoint, this.ClientCredentials);
+        }
+        
+        public KunaServiseClient(System.ServiceModel.Channels.Binding binding, System.ServiceModel.EndpointAddress remoteAddress) : 
+                base(binding, remoteAddress)
+        {
+        }
+        
+        public System.Threading.Tasks.Task<string> GetTimestampAsync()
+        {
+            return base.Channel.GetTimestampAsync();
+        }
+        
+        public virtual System.Threading.Tasks.Task OpenAsync()
+        {
+            return System.Threading.Tasks.Task.Factory.FromAsync(((System.ServiceModel.ICommunicationObject)(this)).BeginOpen(null, null), new System.Action<System.IAsyncResult>(((System.ServiceModel.ICommunicationObject)(this)).EndOpen));
+        }
+        
+        public virtual System.Threading.Tasks.Task CloseAsync()
+        {
+            return System.Threading.Tasks.Task.Factory.FromAsync(((System.ServiceModel.ICommunicationObject)(this)).BeginClose(null, null), new System.Action<System.IAsyncResult>(((System.ServiceModel.ICommunicationObject)(this)).EndClose));
+        }
+        
+        private static System.ServiceModel.Channels.Binding GetBindingForEndpoint(EndpointConfiguration endpointConfiguration)
+        {
+            if ((endpointConfiguration == EndpointConfiguration.BasicHttpBinding_IKunaServise))
+            {
+                System.ServiceModel.BasicHttpBinding result = new System.ServiceModel.BasicHttpBinding();
+                result.MaxBufferSize = int.MaxValue;
+                result.ReaderQuotas = System.Xml.XmlDictionaryReaderQuotas.Max;
+                result.MaxReceivedMessageSize = int.MaxValue;
+                result.AllowCookies = true;
+                return result;
+            }
+            if ((endpointConfiguration == EndpointConfiguration.BasicHttpsBinding_IKunaServise))
+            {
+                System.ServiceModel.BasicHttpBinding result = new System.ServiceModel.BasicHttpBinding();
+                result.MaxBufferSize = int.MaxValue;
+                result.ReaderQuotas = System.Xml.XmlDictionaryReaderQuotas.Max;
+                result.MaxReceivedMessageSize = int.MaxValue;
+                result.AllowCookies = true;
+                result.Security.Mode = System.ServiceModel.BasicHttpSecurityMode.Transport;
+                return result;
+            }
+            throw new System.InvalidOperationException(string.Format("Не удалось найти конечную точку с именем \"{0}\".", endpointConfiguration));
+        }
+        
+        private static System.ServiceModel.EndpointAddress GetEndpointAddress(EndpointConfiguration endpointConfiguration)
+        {
+            if ((endpointConfiguration == EndpointConfiguration.BasicHttpBinding_IKunaServise))
+            {
+                return new System.ServiceModel.EndpointAddress("http://kunaservice/KunaServise.svc");
+            }
+            if ((endpointConfiguration == EndpointConfiguration.BasicHttpsBinding_IKunaServise))
+            {
+                return new System.ServiceModel.EndpointAddress("https://kunaservice.azurewebsites.net/KunaServise.svc");
+            }
+            throw new System.InvalidOperationException(string.Format("Не удалось найти конечную точку с именем \"{0}\".", endpointConfiguration));
+        }
+        
+        public enum EndpointConfiguration
+        {
+            
+            BasicHttpBinding_IKunaServise,
+            
+            BasicHttpsBinding_IKunaServise,
+        }
+    }
+}
---
+++ BotPlatform/BotPlatform/Controllers/KunapublicController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using KunaService;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BotPlatform.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Kunapublic")]
+    public class KunapublicController : Controller
+    {
+        KunaServiseClient kunaServise = new KunaServiseClient();
+
+        // GET: api/Kunapublic/5
+        [HttpGet]
+        public string Get()
+        {
+
+            return "value";
+        }
+        
+        // POST: api/Kunapublic
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+    }
+}
