97a935c7fe83f922c89e4addff76b52b4eae2a4c
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-04-10 19:49:40 +0300

    bug fixes

--- BotPlatform/BotPlatform/Controllers/CryptohackerAnswerController.cs
+++ BotPlatform/BotPlatform/Controllers/CryptohackerAnswerController.cs
@@ -30,7 +30,7 @@ namespace BotPlatform.Controllers
 
             if (attributes.BlockAttribute == "yes no") answer = chatAnswer.GetGenderNameAnswer(attributes.Gender, attributes.FirstName);
 
-            if (attributes.BlockAttribute == "simple set" && attributes.Ref == "MainMenuBlock") answer = chatAnswer.GetDefaultAnswer(attributes.Ref);
+            if (attributes.Ref == "MainMenuBlock") answer = chatAnswer.GetDefaultAnswer(attributes.Ref);
 
             return answer;
         }
